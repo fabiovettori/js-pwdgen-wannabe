43f3438a6ca6c51705d7c502b4dae18b4fd730d4
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-12 18:26:49 +0200

    v.03 end

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,13 @@
-// Password GEN
+// user info
 var name = prompt('Insert your Name');
 var surname = prompt('Insert your Surname');
 var color = prompt('Insert your preferred color');
 
+// user welcome message
 var fullName = name + ' ' + surname;
-console.log(fullName);
 document.getElementById('fullname').innerHTML = 'Hi' + ' ' + fullName;
-document.getElementById('password').innerHTML = name + surname + color + '20';
+
+// password gen
+var password = name + surname + '20';
+var userPassword = password.toLowerCase();
+document.getElementById('password').innerHTML = userPassword;
